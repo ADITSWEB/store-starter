31ac15d09407749ece36b4537f0596b09929a85a
Anupama Deshmukh <aditsweb@gmail.com>
2026-07-24 15:19:18 +0530

    Attempt#2 - deployment to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://<github-username>.github.io",
+  site: "https://github.io",
   base: "/store-starter",
   vite: {
     plugins: [tailwindcss()]
